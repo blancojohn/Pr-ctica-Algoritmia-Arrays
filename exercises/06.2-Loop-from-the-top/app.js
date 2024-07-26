36724a7c9ca6ecce57d78439bd9efc1d4a0908a3
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-07-26 17:59:47 +0000

    práctica Loops

--- exercises/06.2-Loop-from-the-top/app.js
+++ exercises/06.2-Loop-from-the-top/app.js
@@ -1,7 +1,11 @@
 let mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
-
-for(let i = 0; i < mySampleArray.length; i = i + 1)
+/* Solución 1 */
+for(let i = mySampleArray.length -1; i >= 0; i = i - 1)
 {
-	console.log(mySampleArray[i]);
+	console.log("PRIMER", mySampleArray[i]);
 }
+
+
+/* Muestra la cantidad de elementos que contiene el array */
+/* console.log(mySampleArray.length) */
--- exercises/06.4-Loop-from-the-half-to-the-end/app.js
+++ exercises/06.4-Loop-from-the-half-to-the-end/app.js
@@ -1,10 +1,30 @@
 let mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
 
-let initialValue = 0;
-let stopValue = 0;
-let increasingValue = 0;
+/* function printHalfList(array){
+	let stopValue= array.length
+	let initValue= stopValue / 2
+	for (let i= initValue; i < stopValue; i ++){
+		console.log(array[i])
+	}
+}
+
+printHalfList(mySampleArray) */
+
+let initialValue = 7;
+let stopValue = mySampleArray.length;
+let increasingValue = 1;
 
 for(let i = initialValue; i < stopValue; i = i + increasingValue)
 {
 	console.log(mySampleArray[i]);
+} 
+
+/* function cutArrayInHalf(array){
+	let sizeArray= array.length;
+	let halfArray= sizeArray/2;
+	newArray= array.slice(halfArray, sizeArray)
+	console.log(newArray)
 }
+console.log("NUMBER ITEMS:", mySampleArray.length)
+
+cutArrayInHalf(mySampleArray) */
