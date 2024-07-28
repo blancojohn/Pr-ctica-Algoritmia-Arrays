08ba0c1f56abcfc744c27e889bda105158b3e058
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-07-28 21:13:30 +0000

    práctica algoritmia

--- exercises/07.2-Letter-Counter/app.js
+++ exercises/07.2-Letter-Counter/app.js
@@ -3,10 +3,15 @@ let counts = {};
 
 // Your code here
 for (let i= 0; i < par.length; i ++){
-    const letters= par[i]
-    counts[letters]= i++
-    console.log(counts)
-}
-
+    const letters= par[i].toLowerCase()
+    if( letters == " ") continue
+    else if (counts[letters] == undefined){
+        counts[letters]= 1
+   
+        counts[letters]= counts[letters] + 1
+    }
+} 
+/* console.log(par.length) */
+console.log(counts)
 
 
