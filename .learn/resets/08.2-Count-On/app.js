21a28f9867be77b4113886917a713db404843596
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-07-29 17:16:27 +0000

    Insertar en un array solo si es un objeto

--- .learn/resets/08.2-Count-On/app.js
+++ .learn/resets/08.2-Count-On/app.js
@@ -2,13 +2,8 @@ let myArray = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
 let hello = [];
 for(let i = 0; i < myArray.length; i++) {
-    let item = typeof myArray[i];
-    if (item !== "object") continue;
-    else{
-        hello.push(item)
-    }
+    let item = myArray[i];
     // MAGIC HAPPENS HERE
 }
-
 
 console.log(hello)
--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,8 @@
 let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
-
 // Your code here
+let dataTypes= [];
+for (let index in mix){
+    let item= typeof (mix[index])
+    dataTypes.push(item)
+}
+console.log(dataTypes)
