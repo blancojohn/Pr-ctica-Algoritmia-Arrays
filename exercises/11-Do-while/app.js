68e21b63dac2ccd0bb07fe21848487d6c075a8ac
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-07-30 16:51:37 +0000

    Ejecución do...while condicionado

--- exercises/11-Do-while/app.js
+++ exercises/11-Do-while/app.js
@@ -2,6 +2,13 @@ let i = 20;
 
 do {
     // Magic goes here
-
+    let numbers= i
     i--;
+    if(numbers === 0){
+        console.log('LIFTOOF')
+    }else if(numbers % 5 === 0){
+        console.log(numbers + '!')
+    }else{
+        console.log(numbers)
+    }
 } while (i >= 0);
