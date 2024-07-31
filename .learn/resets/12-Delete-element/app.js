8965ee726e9bce04ad1d43242e04e81eab2747cc
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-07-31 14:46:29 +0000

    Función que elimina un elemnto de un array por parámetro con el método filter

--- .learn/resets/12-Delete-element/app.js
+++ .learn/resets/12-Delete-element/app.js
@@ -1,11 +1,6 @@
 let people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
 
-
 // Your code below
-const deletePerson= (name)=>{
-    let newArr= people.filter((names) => names != name)
-    return newArr
-}
 
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
