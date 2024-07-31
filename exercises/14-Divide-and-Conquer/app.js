45ae23f8d02c52d234b2fe037abfa92cf68fe6b8
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-07-31 15:40:09 +0000

    Función itera y retorna un nuevo array de números segmentado entre pares e impares

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,3 +1,14 @@
 let listOfNumbers = [4, 80, 85, 59, 37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68];
 
 // Your code here
+const mergeTwoList= (arr)=>{
+    let odd= [];
+    let even= [];
+    for(let i in arr){
+        (arr[i] % 2 === 0)? even.push(arr[i]) : odd.push(arr[i])
+    }
+    let newArr= odd.concat(even)
+    return newArr 
+}
+
+console.log(mergeTwoList(listOfNumbers))
