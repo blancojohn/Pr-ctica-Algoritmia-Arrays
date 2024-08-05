97f00914a566a2b0cc28348c849b8ee2e2d940a3
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-08-05 14:46:24 +0000

    Creación de nuevos arrays con valores de temperaturas y horas civiles con map

--- exercises/20.1-Map-an-array/app.js
+++ exercises/20.1-Map-an-array/app.js
@@ -1,9 +1,26 @@
 let arrayOfCelsiusValues = [-2, 34, 56, -10];
+let militaryHours = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
+
+/* let militaryToCivilian= function(hour){
+	if(hour == 12) return `${hour}` + "pm";
+	else if(hour == 24) return `${hour-12}` + "am";
+	else if(hour > 11)return `${hour-12}` + "pm";
+	else return hour + "am"
+}
+
+let civilianHours= militaryHours.map(militaryToCivilian)
+
+console.log(civilianHours) */
 
 let arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value) {
-	
 	// Add your code here and return the new value
+	return (value * 9/5) + 32;
 	
 });
 
 console.log(arrayOfFahrenheitValues);
+	
+	
+
+
+
