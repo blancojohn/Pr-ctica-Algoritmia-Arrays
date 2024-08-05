457ddacb1e10c36eb25d856652ee12e199f88b8c
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-08-05 14:57:26 +0000

    map para crear nuevo arreglo con valores multiplicados

--- exercises/20.1-Map-an-array/app.js
+++ exercises/20.1-Map-an-array/app.js
@@ -1,7 +1,7 @@
 let arrayOfCelsiusValues = [-2, 34, 56, -10];
 let militaryHours = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];
 
-/* let militaryToCivilian= function(hour){
+let militaryToCivilian= function(hour){
 	if(hour == 12) return `${hour}` + "pm";
 	else if(hour == 24) return `${hour-12}` + "am";
 	else if(hour > 11)return `${hour-12}` + "pm";
@@ -10,7 +10,7 @@ let militaryHours = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23
 
 let civilianHours= militaryHours.map(militaryToCivilian)
 
-console.log(civilianHours) */
+console.log(civilianHours) 
 
 let arrayOfFahrenheitValues = arrayOfCelsiusValues.map(function(value) {
 	// Add your code here and return the new value
