33930438d16ff0436ce9784332460f4eac6f7a05
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-10-10 14:46:39 +0000

    Mapeando un array para cambiar sus elementos según valor

--- exercises/20.6-Yes-and-no/app.js
+++ exercises/20.6-Yes-and-no/app.js
@@ -1,3 +1,13 @@
 let theBools = [0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
 
 // Your code here
+theNewArray= theBools.map(item=>{
+    if(item == 0){
+        item= 'woko'
+    }
+    else if(item == 1){
+        item= 'wiki'
+    }
+    return item
+})
+console.log(theNewArray)
