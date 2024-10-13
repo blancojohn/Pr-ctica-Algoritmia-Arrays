927db0849f071975c82fb2bc22703b4d84c3059e
John Blanco <142819007+blancojohn@users.noreply.github.com>
2024-10-13 15:39:10 +0000

    Función que recibe un número como parámetro y retorna un array bidimensional con valores aleatorios

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,6 +1,16 @@
-// Your code here
 
-
-
+const matrixBuilder = (number) => {
+    //Crea la primera dimensión del array con longitud de acuerdo al parámetro.
+    let matrix = Array(number)
+    for(let i = 0; i < number; i++) {
+        //Por cada index crea la segunda dimensión momentaneamente con null con longitud del parámetro recibido.
+        matrix[i]= Array(number).fill(null)
+        // Por cada index en la segunda dimensión genera un número aleatorio.
+         for(let j= 0; j < matrix[i].length; j++){
+            matrix[i][j] = Math.floor(Math.random() * 2)
+         }
+    }
+    return matrix
+}
 // Do not change anything from this line down
-console.log(matrixBuilder(5))
+console.log(matrixBuilder(5)) 
